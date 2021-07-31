c7fd427a263f980fc281883e6a097ed1b4b690b3
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2021-07-31 15:35:02 +0200

    Create Clever Zone

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCF1_qwD_r0SFbH66oyc7GdVkegXwrxmEw",
+  authDomain: "moniquechetty-amclone2.firebaseapp.com",
+  projectId: "moniquechetty-amclone2",
+  storageBucket: "moniquechetty-amclone2.appspot.com",
+  messagingSenderId: "147920653429",
+  appId: "1:147920653429:web:78738bdc2850f92ac6fc3c",
+  measurementId: "G-ZMGVPKKBCQ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
